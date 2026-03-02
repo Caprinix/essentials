423281aa0258b734677c80e0dab838d5c387ba7f
replicapra <154707993+replicapra@users.noreply.github.com>
2026-03-02 18:26:15 +0100

    __init__

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = inputs: let
+    lib = inputs.snowfall-lib.mkLib {
+      inherit inputs;
+
+      src = ./.;
+
+      snowfall = {
+        namespace = "caprinix";
+
+        meta = {
+          name = "caprinix-snowfall-lib-starter";
+          title = "Caprinix - snowfall-lib-starter";
+        };
+      };
+    };
+  in
+    lib.mkFlake {
+      outputs-builder = channels: let
+        treefmtEval = inputs.treefmt-nix.lib.evalModule channels.nixpkgs ./treefmt.nix;
+      in {
+        formatter = treefmtEval.config.build.wrapper;
+      };
+    };
+}
